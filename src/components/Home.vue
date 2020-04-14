e864ec4a2cf1af25ae66fd9371cd5a76c31bb913
bruce <bruce1580195886@outlook.com>
2020-04-14 16:57:01 +0800

    登录功能完成

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <el-button type="primary" @click="sign_out">退出登录</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    // 退出，清除token,跳转到登录页面
+    sign_out () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  { path: '/home', component: Home }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+// 挂载路由导航守卫，beforeEach方法
+router.beforeEach((to, from, next) => {
+  // to 要访问的路径，from 表示从哪个路径来，next 是一个函数表示放行
+  if (to.path === '/login') return next()
+  const tokenStr = window.sessionStorage.getItem('token') // 获取token
+  if (!tokenStr) return next('/login') // 强制跳转
+  next()
 })
 
 export default router
